0bf9a0a90c64de8ec719a78e29d8ee45f147b43f
latfa-senatham <56715123+latfa-senatham@users.noreply.github.com>
2020-01-11 16:37:18 +0700

    Create Exercise4_Latfa_S.py

---
+++ Exercise4_Latfa_S.py
@@ -0,0 +1,9 @@
+foundEng = 80
+genBusi = 99
+comSys = 78
+comPro = 85
+print("--- Your Score --- ")
+print("Foundation English : ",foundEng)
+print("General Business : ",genBusi)
+print("Introduction to Computer Systems : ",comSys)
+print("Computer Programming : ",comPro)

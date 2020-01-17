0a9ffe592b735aab18bfca0dfb8e5e91224eed1b
latfa-senatham <56715123+latfa-senatham@users.noreply.github.com>
2020-01-18 01:13:12 +0700

    Create Lecture50_Latfa_S.py

---
+++ Lecture50_Latfa_S.py
@@ -0,0 +1,13 @@
+def sum(x,y):
+    print(x,"+",y,"=",x+y)
+def minus(x,y):
+    print(x,"-",y,"=",x-y)
+def multiply(x,y):
+    print(x,"x",y,"=",x*y)
+def divide(x,y):
+    print(x,"/",y,"=",x/y)
+
+sum(10,5)
+minus(10,5)
+multiply(10,5)
+divide(10,5)
